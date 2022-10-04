c47d72adad45e4fa207871a27cc8929be28dfdae
Patrick M <patrickmacdonald07@gmail.com>
2022-10-04 17:04:13 -0400

    connected front end to back end with get requests

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { animals } = require("./data/animals.json");
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -104,6 +105,22 @@ app.get("/api/animals/:id", (req, res) => {
   } else {
     req.setEncoding(404);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/animals", (req, res) => {
